5ad4e8e477f0a4be73eb0b3d17877c4c2f8b0235
ayman427 <aymankarovadiya1@gmail.com>
2025-02-10 16:54:29 +0530

    Pulse Monitoring Dashboard Added

--- app/Models/User.php
+++ app/Models/User.php
@@ -69,4 +69,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Post::class);
     }
+    public function isAdmin(): bool
+    {
+        return $this->is_admin == 1;
+    }
+
 }
